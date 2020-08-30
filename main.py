358edc489ffd095105dbe193b44bfd9a49e7bd14
gfalkeenthros <58170723+gfalkeenthros@users.noreply.github.com>
2020-08-30 11:21:48 +0200

    tetriminos now land when colliding with grid pieces

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ world.add_processor(CollisionDetectionProcessor(), priority=96)
 
 
 piece_name = random.choice(list(shapes))
-shape = world.create_entity(Shape(shapes[piece_name]), GridPosition(4,0), DeltaPosition(0,0), Speed(0.5), Input(bindings))
+shape = world.create_entity(Shape(shapes[piece_name]), GridPosition(2,0), DeltaPosition(0,0), Speed(0.5), Input(bindings))
 
 while True:
     event_queue.clear()
--- processors.py
+++ processors.py
@@ -1,7 +1,7 @@
 import esper
 import random
 import sys
-import pygame 
+import pygame
 
 from world import world
 from constants import *
@@ -9,12 +9,13 @@ from components import *
 from event_queue import EventQueue
 from events import *
 
+
 class DrawScreenProcessor(esper.Processor):
     def __init__(self):
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
     def process(self):
-        self.screen.fill((0,0,0))
+        self.screen.fill((0, 0, 0))
         for ent, (shape, grid_pos) in world.get_components(Shape, GridPosition):
             self.draw_shape(shape, grid_pos.x, grid_pos.y)
 
@@ -22,25 +23,29 @@ class DrawScreenProcessor(esper.Processor):
         self.draw_grid_overlay()
         pygame.display.update()
 
-    def draw_shape(self,shape, x,y):
+    def draw_shape(self, shape, x, y):
         for i in range(shape.get_height()):
             for j in range(shape.get_width()):
                 if shape.get_current_rotation()[i][j] == 1:
-                    self.draw_block(x*TILE_WIDTH+j*TILE_WIDTH,y*TILE_HEIGHT+i*TILE_HEIGHT, TILE_HEIGHT, TILE_WIDTH)
+                    self.draw_block(
+                        x*TILE_WIDTH+j*TILE_WIDTH, y*TILE_HEIGHT+i*TILE_HEIGHT, TILE_HEIGHT, TILE_WIDTH)
 
     def draw_grid(self):
         for i in range(GRID_HEIGHT):
             for j in range(GRID_WIDTH):
                 if(grid[i][j] != 0):
-                    self.draw_block(j*TILE_WIDTH,i*TILE_HEIGHT,TILE_HEIGHT,TILE_WIDTH)
+                    self.draw_block(j*TILE_WIDTH, i*TILE_HEIGHT,
+                                    TILE_HEIGHT, TILE_WIDTH)
 
-    def draw_block(self,x,y,height,width):
-        pygame.draw.rect(self.screen, (255,0,0), (x,y,width,height))
+    def draw_block(self, x, y, height, width):
+        pygame.draw.rect(self.screen, (255, 0, 0), (x, y, width, height))
 
     def draw_grid_overlay(self):
         for i in range(GRID_HEIGHT):
             for j in range(GRID_WIDTH):
-                pygame.draw.rect(self.screen, (255,255,255), (j*TILE_WIDTH, i*TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT), 2)
+                pygame.draw.rect(self.screen, (255, 255, 255),
+                                 (j*TILE_WIDTH, i*TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT), 2)
+
 
 class InputMapperProcessor(esper.Processor):
     def process(self):
@@ -58,9 +63,10 @@ class InputMapperProcessor(esper.Processor):
     def lookup_binding(self, bindings, key, default=None):
         return bindings.get(pygame.key.name(key), default)
 
+
 class InputProcessor(esper.Processor):
     def process(self):
-        for ent, (input, shape, grid_pos) in world.get_components(Input,Shape, GridPosition):
+        for ent, (input, shape, grid_pos) in world.get_components(Input, Shape, GridPosition):
             if 'QUIT' in input.actions:
                 pygame.quit()
                 sys.exit()
@@ -70,44 +76,56 @@ class InputProcessor(esper.Processor):
             elif 'MOVE_LEFT' in input.actions:
                 grid_pos.x -= 1
                 grid_pos.x = max(grid_pos.x, 0)
-                
+
             elif 'MOVE_RIGHT' in input.actions:
                 grid_pos.x += 1
                 grid_pos.x = min(grid_pos.x, GRID_WIDTH-shape.get_width())
 
             elif 'MOVE_DOWN' in input.actions:
-                grid_pos.y +=1 
+                grid_pos.y += 1
+
 
 class MovePieceProcessor(esper.Processor):
     def process(self):
         for ent, (grid_pos, delta_pos, speed) in world.get_components(GridPosition, DeltaPosition, Speed):
             delta_pos.y += speed.amount * (clock.get_time() / 1000)
             if delta_pos.y >= 1:
-                grid_pos.y += 1 
+                grid_pos.y += 1
                 delta_pos.y = 0
+
 
 class CollisionDetectionProcessor(esper.Processor):
     def process(self):
-        for ent, (grid_pos,shape) in world.get_components(GridPosition, Shape):
+        for ent, (grid_pos, shape) in world.get_components(GridPosition, Shape):
             if grid_pos.x <= 0:
                 event_queue.add(BoundaryHitEvent(LEFT_BOUDNARY))
             elif grid_pos.x >= GRID_WIDTH:
                 event_queue.add(BoundaryHitEvent(RIGHT_BOUNDARY))
 
+
 class LandPieceProcessor(esper.Processor):
     def process(self):
+        hit = False
         for ent, (grid_pos, shape) in world.get_components(GridPosition, Shape):
-            if grid_pos.y + shape.get_height() == GRID_HEIGHT:
+            if grid_pos.y + shape.get_height() >= GRID_HEIGHT:
+                hit = True
+            else:
+                for i in range(grid_pos.y, grid_pos.y + shape.get_height()):
+                    for j in range(grid_pos.x, grid_pos.x + shape.get_width()):
+                        if shape.get_current_rotation()[i-grid_pos.y][j-grid_pos.x] != 0:
+                            if grid[i+1][j] != 0:
+                                hit = True
+            if hit:
                 for i in range(grid_pos.y, grid_pos.y + shape.get_height()):
                     for j in range(grid_pos.x, grid_pos.x + shape.get_width()):
                         if shape.get_current_rotation()[i-grid_pos.y][j-grid_pos.x] != 0:
                             grid[i][j] = 1
-                            event_queue.add(SpawnNewPieceEvent())
-                        
+                event_queue.add(SpawnNewPieceEvent())
                 world.delete_entity(ent)
 
 class SpawnPieceProcessor(esper.Processor):
     def process(self):
         if event_queue.has_event(SpawnNewPieceEvent):
             piece_name = random.choice(list(shapes))
-            shape = world.create_entity(Shape(shapes[piece_name]), GridPosition(4,0), DeltaPosition(0,0), Speed(0.5), Input(bindings))
+            shape = world.create_entity(Shape(shapes[piece_name]), GridPosition(
+                4, 0), DeltaPosition(0, 0), Speed(0.5), Input(bindings))
